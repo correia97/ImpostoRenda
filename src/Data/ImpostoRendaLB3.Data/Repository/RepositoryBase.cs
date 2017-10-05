2cf65256dbff17288a032bd2e6b1a9d469f89e85
PAULO EDUARDO CORREIA <correia97@gmail.com>
2017-10-05 07:23:31 -0300

    Correção de Variavéis

--- src/Data/ImpostoRendaLB3.Data/Repository/RepositoryBase.cs
+++ src/Data/ImpostoRendaLB3.Data/Repository/RepositoryBase.cs
@@ -13,56 +13,58 @@ namespace ImpostoRendaLB3.Data.Repository
     public class RepositoryBase<T> : IRepositoryBase<T> where T : EntityBase
     {
         protected IMongoDatabase DbContext { get; }
+        private  string collectionName { get; set; }
 
         public RepositoryBase(IMongoDBInstance mongoDBInstance)
         {
             if (DbContext == null)
                 DbContext = mongoDBInstance.ReturnDB();
+            collectionName = typeof(T).Name;
         }
 
-        public void Delete(T entity)
+        public async Task Delete(T entity)
         {
             var filter = Builders<T>.Filter.Eq(x => x.Id, entity.Id);
-            DbContext.GetCollection<T>(typeof(T).Name).DeleteOneAsync(filter);
+           await DbContext.GetCollection<T>(collectionName).DeleteOneAsync(filter);
         }
 
         public async Task<T> Find(Guid id)
         {
             var filter = Builders<T>.Filter.Eq(x => x.Id, id);
-            var result = DbContext.GetCollection<T>(typeof(T).Name).Find(filter);
+            var result = DbContext.GetCollection<T>(collectionName).Find(filter);
             return await result.FirstOrDefaultAsync();
         }
 
-        public async Task<T> Get(Expression<Func<T, bool>> prediate)
+        public async Task<T> Get(Expression<Func<T, bool>> predicate)
         {
-            var filter = Builders<T>.Filter.Where(prediate);
-            var result = DbContext.GetCollection<T>(typeof(T).Name).Find(filter);
+            var filter = Builders<T>.Filter.Where(predicate);
+            var result = DbContext.GetCollection<T>(collectionName).Find(filter);
             return await result.FirstOrDefaultAsync();
         }
 
         public async Task<List<T>> GetAll()
         {
-            var result = DbContext.GetCollection<T>(typeof(T).Name).AsQueryable();
+            var result = DbContext.GetCollection<T>(collectionName).AsQueryable();
             return await result.ToListAsync();
         }
 
-        public async Task<List<T>> GetAll(Expression<Func<T, bool>> prediate)
+        public async Task<List<T>> GetAll(Expression<Func<T, bool>> predicate)
         {
-            var filter = Builders<T>.Filter.Where(prediate);
-            var result = await DbContext.GetCollection<T>(typeof(T).Name).FindAsync(filter);
+            var filter = Builders<T>.Filter.Where(predicate);
+            var result = await DbContext.GetCollection<T>(collectionName).FindAsync(filter);
             return await result.ToListAsync();
         }
 
-        public void Insert(T entity)
+        public async Task Insert(T entity)
         {
-            DbContext.GetCollection<T>(typeof(T).Name).InsertOneAsync(entity);
+            await DbContext.GetCollection<T>(collectionName).InsertOneAsync(entity);
         }
 
-        public void Update(T entity)
+        public async Task Update(T entity)
         {
             var filter = Builders<T>.Filter.Eq(x => x.Id, entity.Id);
-            DbContext.GetCollection<T>(typeof(T).Name)
-                                       .ReplaceOneAsync(filter, entity, new UpdateOptions { IsUpsert = true });
+            await DbContext.GetCollection<T>(collectionName)
+                                         .ReplaceOneAsync(filter, entity, new UpdateOptions { IsUpsert = true });
         }
     }
 }
--- src/Domain/ImpostoRendaLB3.Domain/Interfaces/Repository/IRepositoryBase.cs
+++ src/Domain/ImpostoRendaLB3.Domain/Interfaces/Repository/IRepositoryBase.cs
@@ -8,12 +8,12 @@ namespace ImpostoRendaLB3.Domain.Interfaces.Repository
 {
     public interface IRepositoryBase<T> where T : class
     {
-        Task<T> Get(Expression<Func<T, bool>> prediate);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
         Task<List<T>> GetAll();
-        Task<List<T>> GetAll(Expression<Func<T, bool>> prediate);
+        Task<List<T>> GetAll(Expression<Func<T, bool>> predicate);
         Task<T> Find(Guid id);
-        void Update(T entity);
-        void Insert(T entity);
-        void Delete(T entity);
+        Task Update(T entity);
+        Task Insert(T entity);
+        Task Delete(T entity);
     }
 }
