a01783d2f8c81ef6210c4c889030651dfc1a9fa5
Paulo Eduardo Correia <paulo.correia@docway.co>
2020-02-08 14:01:46 -0300

    using

--- src/CrossCutting/ImpostoRendaLB3.CrossCutting.IoC/BootStrapper.cs
+++ src/CrossCutting/ImpostoRendaLB3.CrossCutting.IoC/BootStrapper.cs
@@ -4,9 +4,6 @@ using ImpostoRendaLB3.Domain.Interfaces.Repository;
 using ImpostoRendaLB3.Domain.Interfaces.Service;
 using ImpostoRendaLB3.Domain.Service;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ImpostoRendaLB3.CrossCutting.IoC
 {
