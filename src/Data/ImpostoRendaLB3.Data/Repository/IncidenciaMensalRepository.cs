ce3a772333052e6a9aaa17735f8f6200831edad5
Paulo Eduardo Correia <paulo.correia@docway.co>
2020-06-02 19:59:34 -0300

    novo teste da pipeline circleci e github

--- src/Data/ImpostoRendaLB3.Data/Repository/IncidenciaMensalRepository.cs
+++ src/Data/ImpostoRendaLB3.Data/Repository/IncidenciaMensalRepository.cs
@@ -2,8 +2,10 @@
 using ImpostoRendaLB3.Domain.Interfaces.Entities;
 using ImpostoRendaLB3.Domain.Interfaces.Repository;
 using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace ImpostoRendaLB3.Data.Repository
@@ -18,6 +20,13 @@ namespace ImpostoRendaLB3.Data.Repository
 
         public async Task<IIncidenciaMensal> RetornaIncidenciaMensalPorSalario(decimal salario)
         {
+
+            Expression<Func<IncidenciaMensal, bool>> predicate = (x) => salario >= x.ValorInicial && salario <= x.ValorFinal;
+            var filter = Builders<IncidenciaMensal>.Filter.Where(predicate);
+
+            var result1 = await DbContext.GetCollection<IncidenciaMensal>(collectionName).Find(filter).ToListAsync();
+            var yyyy = result1.FirstOrDefault();
+
             var result = await DbContext.GetCollection<IncidenciaMensal>(collectionName).Find(x => true).ToListAsync();
             var incidencia = result.FirstOrDefault(x => salario >= x.ValorInicial && salario <= x.ValorFinal);
             if (incidencia != null)
--- src/Domain/ImpostoRendaLB3.Domain/Service/ImpostoService.cs
+++ src/Domain/ImpostoRendaLB3.Domain/Service/ImpostoService.cs
@@ -24,7 +24,7 @@ namespace ImpostoRendaLB3.Domain.Service
                 await _incidenciaMensalRepository.Insert(new IncidenciaMensal(1903.99M, 2826.65M, 7.5M, 142.8M));
                 await _incidenciaMensalRepository.Insert(new IncidenciaMensal(2826.66M, 3751.05M, 15M, 354.8M));
                 await _incidenciaMensalRepository.Insert(new IncidenciaMensal(3751.06M, 4664.68M, 22.5M, 636.13M));
-                await _incidenciaMensalRepository.Insert(new IncidenciaMensal(4664.69M, decimal.MaxValue, 27.5M, 869.36M));
+                await _incidenciaMensalRepository.Insert(new IncidenciaMensal(4664.69M, 9999999.99M, 27.5M, 869.36M));
             }
         }
 
